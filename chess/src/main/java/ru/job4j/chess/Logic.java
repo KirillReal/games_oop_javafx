e686b60230ac141d9911414299bc49787e24e87e
KirillReal <kiril5471710@gmail.com>
2020-10-27 17:47:57 +0300

    2. Каркас шахматной доски [#348447]

--- chess/src/main/java/ru/job4j/chess/Logic.java
+++ chess/src/main/java/ru/job4j/chess/Logic.java
@@ -26,7 +26,7 @@ public final class Logic {
         for (Figure figure: figures)
         {
             for (Cell cell:steps) {
-                if(figure.position() == cell){
+                if(figure.position().equals(cell)){
                     throw new OccupiedCellException();
                 }
             }
